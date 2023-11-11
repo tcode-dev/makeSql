6f03b823afdf2e709786d24c5401506ca3b33279
tcode <tk@tcode.dev>
2023-11-11 21:57:05 +0900

    Update App.vue

--- makesql/src/App.vue
+++ makesql/src/App.vue
@@ -9,7 +9,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-      <p>test</p>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
