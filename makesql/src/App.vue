03855e1e8aa41885e0477e7dc052480f00cc8e3e
tcode <tk@tcode.dev>
2023-11-13 21:44:16 +0900

    create TextView

--- makesql/src/App.vue
+++ makesql/src/App.vue
@@ -10,7 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Text</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
---
+++ makesql/src/views/TextView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2 class="h5 mb-4">TextView</h2>
+  </div>
+</template>
