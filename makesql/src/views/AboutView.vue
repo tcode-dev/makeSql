f74da34e679765e23327ab30dc72e757cfb3c2b3
tcode <tk@tcode.dev>
2023-11-15 22:16:41 +0900

    remove bootstrap classname

--- makesql/src/views/AboutView.vue
+++ makesql/src/views/AboutView.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <h2 class="h5 mb-4">このサイトについて</h2>
-    <section class="mb-4">
-      <h3 class="h5 font-weight-bold">動作環境について</h3>
+    <h2>このサイトについて</h2>
+    <section>
+      <h3>動作環境について</h3>
         <p>当サイトは以下の環境での動作を確認しています。</p>
         <dl>
           <dt>OS</dt>
@@ -14,12 +14,12 @@
         </dl>
         <p>いずれもブラウザの設定でCSSとJavaScriptが有効な状態になっているものとします。</p>
     </section>
-    <section class="mb-4">
-      <h3 class="h5 font-weight-bold">ご利用について</h3>
+    <section>
+      <h3>ご利用について</h3>
       <p>当サイトのご利用については自己責任でお願いします。</p>
     </section>
     <section class="mb-4">
-      <h3 class="h5 font-weight-bold">お問い合わせ</h3>
+      <h3>お問い合わせ</h3>
       <address>
         <p>下記メールアドレスよりお問い合わせください。</p>
         <p>&#109;&#97;&#107;es&#113;&#108;&#64;&#103;&#109;&#97;&#105;l&#46;c&#111;m</p>
