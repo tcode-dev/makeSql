1d16ac70ccd0ab4f061f2b12f947058938e1426c
tcode <tk@tcode.dev>
2023-11-21 08:51:31 +0900

    updelete text index.vue

--- makesql/src/views/TextView.vue
+++ makesql/src/views/TextView.vue
@@ -1,8 +1,14 @@
-<script setup lang="ts">
-import Text from '../components/page/text/Index.vue'
-</script>
-
 <template>
-  <Text />
+  <div>
+    <h2>テキストからSQLを作成</h2>
+    <div>
+      <div>
+        <SelectButton />
+      </div>
+    </div>
+  </div>
 </template>
 
+<script setup lang="ts">
+import SelectButton from '../components/page/text/SelectButton.vue'
+</script>
