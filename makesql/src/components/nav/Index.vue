227b0ebfbaa55a2d152cd3f1f59e9e9ea07d7825
tcode <tk@tcode.dev>
2023-11-25 20:43:06 +0900

    nav

--- makesql/src/components/nav/Index.vue
+++ makesql/src/components/nav/Index.vue
@@ -1,10 +1,10 @@
+<template>
+<nav class="flex justify-center space-x-4">
+  <div class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"><RouterLink to="/">Text</RouterLink></div>
+  <div class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"><RouterLink to="/about">About</RouterLink></div>
+</nav>
+</template>
+
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
 </script>
-
-<template>
-  <nav>
-    <RouterLink to="/">Text</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-  </nav>
-</template>
