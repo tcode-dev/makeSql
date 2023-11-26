ba0a35abb57e76355d3f04f344639f80998add08
tcode <tk@tcode.dev>
2023-11-27 07:32:54 +0900

    configStore

--- makesql/src/stores/configStore.ts
+++ makesql/src/stores/configStore.ts
@@ -1,8 +1,15 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
+type Config = {
+  lettercase: string,
+  delimiter: string,
+  bulk: boolean,
+  quotation: string,
+};
+
+export const useConfigStore = defineStore('config', () => {
+  const count = ref({})
   const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
