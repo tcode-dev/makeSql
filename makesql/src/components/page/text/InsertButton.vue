19a107ddcbad74a689bc8d6cf8c3013fdc6a457d
tcode <tk@tcode.dev>
2023-12-02 17:48:11 +0900

    Create InsertButton.vue

---
+++ makesql/src/components/page/text/InsertButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="btn" @click="click">Insert</button>
+</template>
+
+<script setup lang="ts">
+import MakeInsert from '../../../sql/MakeInsert'
+import { useTextStore } from '@/stores/textStore'
+import { useConfigStore } from '@/stores/configStore'
+
+const textStore = useTextStore()
+const configStore = useConfigStore()
+const click = () => {
+  const makeInsert = new MakeInsert({
+    tableName: textStore.state.tableName,
+    contents: textStore.state.contents,
+    config: configStore.config
+  });
+  textStore.setSql(makeInsert.make())
+}
+
+</script>
