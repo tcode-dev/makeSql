6caa45905778f9a20fa0d7898547cc92bb24505c
tcode <tk@tcode.dev>
2023-12-03 10:31:15 +0900

    Create Button.vue

---
+++ makesql/src/components/button/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900">{{label}}</button>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface Props {
+  label: string
+}
+
+defineProps<Props>()
+</script>
