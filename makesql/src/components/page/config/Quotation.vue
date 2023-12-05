6708c73dac483055e45a71c4a702782bebe1b067
tcode <tk@tcode.dev>
2023-12-06 08:35:08 +0900

    config Quotation

---
+++ makesql/src/components/page/config/Quotation.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useConfigStore } from '@/stores/configStore'
+
+const store = useConfigStore()
+</script>
+
+<template>
+  <tr>
+    <th scope="row">quotation</th>
+    <td colspan="3">
+      <div>
+        <input type="checkbox" name="quotation" v-model="store.state.quotation">
+      </div>
+    </td>
+  </tr>
+</template>
