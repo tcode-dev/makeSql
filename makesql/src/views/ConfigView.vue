eb9d918ab1712058bbee0d558bbc83165cb04907
tcode <tk@tcode.dev>
2023-12-06 08:35:14 +0900

    config

--- makesql/src/views/ConfigView.vue
+++ makesql/src/views/ConfigView.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import Bulk from '../components/page/config/Bulk.vue'
+import CharacterCode from '../components/page/config/CharacterCode.vue'
 import Delimiter from '../components/page/config/Delimiter.vue'
+import Lettercase from '../components/page/config/Lettercase.vue'
+import Quotation from '../components/page/config/Quotation.vue'
 </script>
 
 <template>
@@ -9,7 +12,10 @@ import Delimiter from '../components/page/config/Delimiter.vue'
     <table>
       <tbody>
         <Delimiter />
+        <Lettercase />
+        <CharacterCode />
         <Bulk />
+        <Quotation />
       </tbody>
     </table>
   </div>
